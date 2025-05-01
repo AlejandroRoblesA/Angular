53132df3e7df4377c0bfa79437e125e3b59b5c27
Alejandro Robles <alejandro_r1704@hotmail.com>
2025-04-30 18:04:03 -0600

    Adding more components to the demo-app

---
+++ Angular - The Complete Guide 2025/01-starting-project/src/app/tasks/task/task.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css',
+})
+export class TaskComponent {}
