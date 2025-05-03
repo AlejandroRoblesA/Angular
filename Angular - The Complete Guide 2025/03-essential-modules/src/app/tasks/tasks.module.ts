5d768814fb310b2f0229a9260b95d2f36b881714
Alejandro Robles <alejandro_r1704@hotmail.com>
2025-05-02 18:10:58 -0600

    Add missing import

--- Angular - The Complete Guide 2025/03-essential-modules/src/app/tasks/tasks.module.ts
+++ Angular - The Complete Guide 2025/03-essential-modules/src/app/tasks/tasks.module.ts
@@ -5,10 +5,11 @@ import { TasksComponent } from './tasks.component';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TasksComponent, TaskComponent, NewTaskComponent],
   exports: [TasksComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, FormsModule, SharedModule],
 })
 export class TasksModule {}
